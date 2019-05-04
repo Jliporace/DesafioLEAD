5a2083d60ecf9ed66bd8bd4d9085fe2fee4af8d4
JessicaLiporace <jliporace@poli.ufrj.br>
2019-05-04 16:43:58 -0300

    Add files main.cpp and functions.h

---
+++ functions.h
@@ -0,0 +1,31 @@
+/* This file contains the functions to be used in the main code for the Desafio LEAD projetc*/
+
+bool readSensorLog(float data)
+//Reads the data stored in file and stores it in data matrix
+{
+	printf("Not implemented yet");
+	return 0;
+}
+
+bool modulation(int rate, float data[])
+// Process the sensor data with a modulation of <rate> times. Creates and writes results in file modulation.log
+{
+	printf("Not implemented yet");
+	return 0;
+
+}
+
+
+bool movingAverage(int numSamples, float data[])
+// Process the sensor data with a moving average of <numSamples> samples. Creates and writes results in file movingAverage.log
+{
+	printf("Not implemented yet");
+	return 0;
+}
+
+bool lowPassFilter(float tau, float period, float data[])
+// Passes a low pass filter to the data with constant <tau> and sampling period <period>. Creates and writes results in file lowpass.log
+{
+	printf("Not implemented yet");
+	return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*									MainDesafioIC.cpp
+
+This program was created in order to solve the proposed challenge by LEAD lab and contains the following functions in functions.h:
+readSensorLog(), modulation(), movingAverage(), lowPassFilter() e main().
+
+Using the functions above, the program processes sensor data, stored in the file sensor.log, and cretaes three new files:
+- modulation.log: sensor data with a 2x modulation
+- mean.log: sensor data following a moving average of 20 samples
+- lowpass.log: sensor data processed by a low pass filter with tau = 0.1
+
+The file sensor.log has data displayed in two columns: the first element of each row corresponds to the timestamp of the information
+and the second element represents the actual data acquired in that time.
+*/
+
+#include <iostream>
+#include "functions.h"
+ 
+
+int main() {
+	
+	return 0;
+}
