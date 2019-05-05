893356cfb01ec0c2c6a7a9c086f50a5bf530a27f
JessicaLiporace <jliporace@poli.ufrj.br>
2019-05-04 22:13:13 -0300

    Develop function modulation and remove function readSensorLog

--- functions.h
+++ functions.h
@@ -1,29 +1,47 @@
-/* This file contains the functions to be used in the main code for the Desafio LEAD projetc*/
+/* This file contains the functions to be used in the main code for the Desafio LEAD project*/
 
-bool readSensorLog(float data)
-//Reads the data stored in file and stores it in data matrix
-{
-	printf("Not implemented yet");
-	return 0;
-}
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string> 
+using namespace std;
 
-bool modulation(int rate, float data[])
+
+bool modulation(int rate, vector<float> fullData)
 // Process the sensor data with a modulation of <rate> times. Creates and writes results in file modulation.log
-{
-	printf("Not implemented yet");
-	return 0;
-
+// The vector fullData represents all the data in sensor.log file, pushed into the vector line by line: 
+// the even elements represent the timestamp and the odd elements represent sensordata.
+{	
+	fstream file;
+	file.open("modulation.log", ios::out);
+	if(file.is_open())
+	{
+		printf("Creating modulation.log file...");
+	}
+	else 
+	{
+		printf("Couldn't open modulation.log file'");
+		return 0;
+	}
+	for (int i = 1; i<fullData.size(); i = i+2)
+	{
+		file << fullData[i-1] << " " << fullData[i]*rate << endl;
+		
+	}
+	file.close();
+	return 1;
 }
 
 
-bool movingAverage(int numSamples, float data[])
+bool movingAverage(int numSamples, float data[], int m)
 // Process the sensor data with a moving average of <numSamples> samples. Creates and writes results in file movingAverage.log
 {
 	printf("Not implemented yet");
 	return 0;
 }
 
-bool lowPassFilter(float tau, float period, float data[])
+bool lowPassFilter(float tau, float period, float data[], int m)
 // Passes a low pass filter to the data with constant <tau> and sampling period <period>. Creates and writes results in file lowpass.log
 {
 	printf("Not implemented yet");
