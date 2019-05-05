663717a921c5328f4bf635c4e0c898824486eb85
JessicaLiporace <jliporace@poli.ufrj.br>
2019-05-05 16:11:10 -0300

    Add file plotData.m

---
+++ plotData.m
@@ -0,0 +1,29 @@
+%                           LEAD Challenge
+% Plot data from files mean.log, lowpass.log, modulation.log and sensor.log
+
+mean = load("mean.log");
+lowpass = load("lowpass.log");
+modulation = load("modulation.log");
+sensor = load("sensor.log");
+out = load("out.log");
+
+
+timestamp = mean(:,1);
+mean = mean(:,2);
+sensor = sensor(:,2);
+lowpass = lowpass(:,2);
+modulation = modulation(:,2);
+out = out(:,2);
+
+plot(timestamp, sensor);
+hold on;
+plot(timestamp, mean);
+hold on;
+plot(timestamp, lowpass);
+hold on;
+plot(timestamp, modulation);
+hold on;
+plot(timestamp, out);
+title('LEAD challenge: modulation, movingAverage and lowPassFilter');
+legend({'y = sensor','y = mean', 'y = lowPassFilter','y = modulation', 'y = out'}, 'Location','southeast');
+
